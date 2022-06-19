47416fd9fb847462f17e2987034ba1972c23d196
Harsh6575 <harsh.190410107146@gmail.com>
2022-06-19 10:02:18 +0530

    footer

--- src/COMPONENTS/Footer.js
+++ src/COMPONENTS/Footer.js
@@ -1,18 +1,10 @@
 import './My.css'
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
     return (
         <div className='f-container'>
-            <div>
-                Quicklinks
-                <div className='q-links'>
-                    <Link to='/' className='q-l'>Home</Link>
-                    <Link to='/Products' className='q-l'>Products</Link>
-                    <Link to='/Contact' className='q-l'>Contact</Link>
-                </div>
-            </div>
             <div className='last-line'>
                 This web site is made with <a className='f-links' href="https://reactjs.org/">React.</a>
             </div>
